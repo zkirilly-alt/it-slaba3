8b9ba09aa7f0c89300cee99e30c512c036d6079f
zkirilly-alt <z.kirill.y@gmail.com>
2025-12-18 15:12:35 +0300

    вся програма

--- inout.h
+++ inout.h
@@ -7,15 +7,15 @@
 void allinout(void);
 
 void print_menu(void);
-void process_command_line(int argc, char* argv[]);  
+void process_command_line(int argc, char *argv[]);
 
-void process_file_mode(const char* filename); 
+void process_file_mode(const char *filename);
 
-int safe_fgets(char* buffer, int size, FILE* stream);
-int is_valid_number(const char* str);
-Deque* create_deque_from_input(const char* input);
+int safe_fgets(char *buffer, int size, FILE *stream);
+int is_valid_number(const char *str);
+Deque *create_deque_from_input(const char *input);
 
 void test_all_files(void);
 void create_tests_directory(void);
 
-#endif 
+#endif
